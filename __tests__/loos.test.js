babcda95fe8dfedc58510655c09e15909f761600
Steven Bryant <stevelad_83@yahoo.com>
2023-02-21 15:31:46 -0500

    built POST route for loo

--- __tests__/loos.test.js
+++ __tests__/loos.test.js
@@ -2,35 +2,35 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-// const UserService = require('../lib/services/UserService');
+const UserService = require('../lib/services/UserService');
 
-// const mockUser = {
-//   firstName: 'Test',
-//   lastName: 'User',
-//   email: 'test@example.com',
-//   password: '12345',
-// };
+const mockUser = {
+  username: 'User',
+  email: 'test@example.com',
+  password: '12345',
+};
 
-// const mockLoo = {
-//   description: 'This loo is nice!',
-//   rating: 5,
-// };
+const mockLoo = {
+  description: 'This loo is nice!',
+  rating: '5',
+  review_id: null,
+};
 
-// const registerAndLogin = async (userProps = {}) => {
-//   const password = userProps.password ?? mockUser.password;
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
 
-//   // Create an "agent" that gives us the ability
-//   // to store cookies between requests in a test
-//   const agent = request.agent(app);
+  // Create an "agent" that gives us the ability
+  // to store cookies between requests in a test
+  const agent = request.agent(app);
 
-//   // Create a user to sign in with
-//   const user = await UserService.create({ ...mockUser, ...userProps });
+  // Create a user to sign in with
+  const user = await UserService.create({ ...mockUser, ...userProps });
 
-//   // ...then sign in
-//   const { email } = user;
-//   await agent.post('/api/v1/users/sessions').send({ email, password });
-//   return [agent, user];
-// };
+  // ...then sign in
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
+};
 
 describe('loo routes', () => {
   beforeEach(() => {
@@ -40,15 +40,16 @@ describe('loo routes', () => {
     pool.end();
   });
 
-  // it('POST api/v1/loos should create a new loo', async () => {
-  //   const res = await request(app).post('/api/v1/loos').send(mockLoo);
-  //   expect(res.status).toBe(200);
-  //   expect(res.body).toEqual({
-  //     id: expect.any(String),
-  //     created_at: expect.any(String),
-  //     ...mockLoo,
-  //   });
-  // });
+  it('POST api/v1/loos should create a new loo', async () => {
+    const [agent] = await registerAndLogin();
+    const res = await agent.post('/api/v1/loos').send(mockLoo);
+    // expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      created_at: expect.any(String),
+      ...mockLoo,
+    });
+  });
 
   it('GET api/v1/loos should return a list of loos', async () => {
     const res = await request(app).get('/api/v1/loos');
